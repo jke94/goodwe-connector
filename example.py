7de9d5b8f0e66709ee428280f2bbe097ceca551c
jke94 <jcarracedo.dev@gmail.com>
2023-02-21 20:07:32 +0100

    Add configparser information.

--- example.py
+++ example.py
@@ -1,10 +1,19 @@
 from goodwe_connector.goodwe_api import GoodweApi
 import datetime
+import configparser
+
+# Read configuration
+config = configparser.ConfigParser()
+config.read('goodw_api_config.json')
+
+system_id = config['goowdw_api_connection']['system_id']
+user = config['goowdw_api_connection']['account']
+password = config['goowdw_api_connection']['password']
 
 goodweapi = GoodweApi(
-    system_id='',
-    account='',
-    password='')
+    system_id=system_id,
+    account=user,
+    password=password)
 
 data = goodweapi.dummy_function(
     "v2/PowerStationMonitor/GetPowerStationPowerAndIncomeByDay",

7664a80dc4adb024eb9181f11435335cbbbd2eab
jke94 <jcarracedo.dev@gmail.com>
2023-02-23 21:58:09 +0100

    Read config.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # goodwe-connector
 A python library to connect with Goodwe API Rest and get production data about solar panels.
+
+## Goodwe connector user configurarion.
+
+- System Id
+- Account
+- Password
+
+Example of goodwe_config.json
+
+```
+{
+    "goodwe_api_connection":{
+        "system_id":"",
+        "account":"",
+        "password":""
+    }
+}
+```
--- example.py
+++ example.py
@@ -1,23 +1,34 @@
 from goodwe_connector.goodwe_api import GoodweApi
 import datetime
 import configparser
+import os.path
 
-# Read secrets configuration file
-config = configparser.ConfigParser()
-config.read('goodwe_config.json')
+def main():
 
-# Get credentials
-system_id = config['goowdw_api_connection']['system_id']
-user = config['goowdw_api_connection']['account']
-password = config['goowdw_api_connection']['password']
+    config_file = 'goodwe_config.json'
+    
+    if(not os.path.exists(config_file)):
+        print(f'Error! File: {config_file}, does not exits! The file needs store the secrets for the connection.')
+        return None
 
-goodweapi = GoodweApi(
-    system_id=system_id,
-    account=user,
-    password=password)
+    # Read secrets configuration file
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    
+    system_id = config['goowdw_api_connection']['system_id']
+    user = config['goodwe_api_connection']['account']
+    password = config['goodwe_api_connection']['password']
 
-data = goodweapi.dummy_function(
-    "v2/PowerStationMonitor/GetPowerStationPowerAndIncomeByDay",
-    datetime.datetime(2023,2,19))
+    goodweapi = GoodweApi(
+        system_id=system_id,
+        account=user,
+        password=password)
 
-print(data)
+    data = goodweapi.dummy_function(
+        "v2/PowerStationMonitor/GetPowerStationPowerAndIncomeByDay",
+        datetime.datetime(2023,2,19))
+
+    print(data)
+
+if __name__ == "__main__":
+    main()

cc37876b556f948d5af33b3a2c04dfab95d7d542
jke94 <jcarracedo.dev@gmail.com>
2023-06-25 12:30:00 +0200

    Format string.

--- example.py
+++ example.py
@@ -33,7 +33,7 @@ def __get_power_generation_between_dates(goodweapi:GoodweApi) -> None:
     
     data = goodweapi.get_power_generation_between_dates(
         start_date=datetime.datetime(2023, 6, 1),
-        end_date=datetime.datetime(2023, 6, 23)
+        end_date=datetime.datetime(2023, 6, 30)
     )
     
     print(json.dumps(data, indent = 4))
@@ -75,7 +75,7 @@ def main():
     print('Uncomment some function to run it! ;)')
 
     # __get_power_generation_per_day(goodweapi)
-    # __get_power_generation_between_dates(goodweapi)
+    __get_power_generation_between_dates(goodweapi)
     # __get_power_station_generated_every_five_minutes_per_day(goodweapi)
     # __get_power_station_monitor_detail(goodweapi, year=2022, month=12)
     
